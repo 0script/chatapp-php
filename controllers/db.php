213973134f420f63863d1a826a098fd393a54f97
0script <z5r00script@gmail.com>
2022-12-05 00:51:46 +0200

    removed creds

--- controllers/db.php
+++ controllers/db.php
@@ -4,5 +4,5 @@
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
     // connection to DB
-    $conn=mysqli_connect("localhost","phpmyadmin","Admin@Php#1234","chatappdb") or die(mysqli_error($conn));
+    $conn=mysqli_connect("localhost","usename","passwd","dbname") or die(mysqli_error($conn));
 ?>
